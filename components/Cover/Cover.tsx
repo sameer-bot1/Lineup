44e09e299085a2045f26292077dac0baf6b11d8d
sameer <sameerbhardiya.com>
2024-03-19 19:07:40 +0530

    cover

--- components/Cover/Cover.tsx
+++ components/Cover/Cover.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 const Cover = () => {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className='border-blue-500 bg-gradient-to-r from-indigo-900 to-indigo-950 flex flex-col md:flex-row w-full h-screen items-center relative border-b-2'>
       <div className='bg-gradient-to-r from-lime-950 to-lime-900 w-full lg:w-96 h-96 lg:h-auto  lg:ml-72 p-10 lg:mr-11 rounded-2xl lg:flex-none lg:flex-shrink-0'>
@@ -13,18 +26,22 @@ const Cover = () => {
             height={80}
           />
         </div>
-        <div>
-          <p className='text-white text-sm mt-9'>Carpe Diem Pension, a blockchain-based retirement fund, offers permanent payouts through CDP deposits, with a 4.32% annual inflation claimable by depositors.</p>
+        {windowWidth >= 768 && (
+          <div>
+            <p className='text-white text-sm mt-9'>Carpe Diem Pension, a blockchain-based retirement fund, offers permanent payouts through CDP deposits, with a 4.32% annual inflation claimable by depositors.</p>
+          </div>
+        )}
+      </div>
+      {windowWidth >= 768 && ( 
+        <div className='bg-white absolute right-2/4 top-48 rounded '>
+          <Image  
+            src='/bg.png'
+            alt='logo'
+            width={155}
+            height={30}
+          />
         </div>
-      </div>
-      <div className='bg-white absolute right-2/4 top-48 rounded '>
-        <Image  
-          src='/bg.png'
-          alt='logo'
-          width={155}
-          height={30}
-        />
-      </div>
+      )}
       <div className='w-full md:w-2/5 ml-4 mt-8 md:mt-0'>
         <p className='text-3xl font-medium text-white'>Securing Trust: Boosting Security and Strengthening Trust at Carpe Diem Pension</p>
         <p className='text-white text-sm mt-6'>QuillAudits enhances Carpe Diem Pension by addressing 33 vulnerabilities on the Pulse blockchain, boosting security and user trust in digital pensions.</p>
